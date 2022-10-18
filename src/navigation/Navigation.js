e42c27ed9f8518e06a4d3a32fc5dc7f6b413d121
Silius0312 <silius0312@gmail.com>
2022-10-18 15:30:36 +0300

    First commit

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Stack = createNativeStackNavigator();
+
+const ScreenLogIn = () => {
+    return (
+        <Stack.Navigator
+        
+    )
+}
